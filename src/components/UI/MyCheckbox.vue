3428ac84ee9131baf5592826ad09b57ca253a7bd
Emil Burganov <emil.burganov03@gmail.com>
2023-05-21 21:22:54 +0300

    Initial commit

--- src/components/UI/MyCheckbox.vue
+++ src/components/UI/MyCheckbox.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <input type="checkbox"
+    <input :checked="checked" type="checkbox"
            class="cursor-pointer w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500">
     <label class="text-sm font-medium text-gray-900"></label>
   </div>
 </template>
+
+<script setup>
+defineProps(['checked']);
+</script>
